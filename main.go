bad18b869d6d453a0a2a02132779905ab50663d3
Moonrisefire <vitalijsibrin@gmail.com>
2025-08-11 21:11:25 +0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	timeout := flag.Duration("timeout", 10*time.Second, "connection timeout")
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host port [--timeout=10s]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	address := net.JoinHostPort(host, port)
+
+	conn, err := net.DialTimeout("tcp", address, *timeout)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Connection error: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	fmt.Printf("Connected to %s\n", address)
+
+	done := make(chan struct{})
+
+	go func() {
+		_, err := io.Copy(os.Stdout, conn)
+		if err != nil && !isClosedNetworkError(err) {
+			fmt.Fprintf(os.Stderr, "Read error: %v\n", err)
+		}
+		close(done)
+	}()
+
+	go func() {
+		_, err := io.Copy(conn, os.Stdin)
+		if err != nil && !isClosedNetworkError(err) {
+			fmt.Fprintf(os.Stderr, "Write error: %v\n", err)
+		}
+		if tcpConn, ok := conn.(*net.TCPConn); ok {
+			tcpConn.CloseWrite()
+		}
+	}()
+
+	<-done
+	fmt.Println("\nConnection closed.")
+}
+
+func isClosedNetworkError(err error) bool {
+	if err == io.EOF {
+		return true
+	}
+	if netErr, ok := err.(net.Error); ok && !netErr.Timeout() {
+		return true
+	}
+	return false
+}
